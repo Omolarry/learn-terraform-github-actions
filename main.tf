473fade03eaa33d02689a97c81b73be17ed02198
Omolarry <tomikefamoroti@gmail.com>
2022-09-07 00:26:24 +0100

    credentials

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.0"
-   }
+      source = "hashicorp/aws"
+    }
   }
- }
+}
 
-
+provider "aws" {
+  region  = "eu-west-3"
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files = ["~/.aws/config"]
+  profile = "default"
+}
+terraform {
   cloud {
     organization = "Demo-Organization-tf"
 
@@ -15,9 +20,6 @@ terraform {
       name = "gh-actions-demo"
     }
   }
-
-provider "aws" {
-  region = "eu-west-3"
 }
 
 resource "random_pet" "sg" {}
